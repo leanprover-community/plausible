8534b46d2aa58845d096e6b58d19359cd21a9b59
ngernest <eyn5@cornell.edu>
2025-07-23 14:02:30 -0400

    Remove Chamelean files that are not relevant to deriving Arbitrary

--- Plausible.lean
+++ Plausible.lean
@@ -13,11 +13,6 @@ import Plausible.Tactic
 
 -- TODO: remove these imports in production code
 -- (these are here so that `lake build` compiles the new metaprogramming code)
-import Plausible.IR.PlausibleIR
-
--- import Plausible.NKI.Basic
--- import Plausible.NKI.Operators
--- import Plausible.NKI.TypeCheck
--- import Plausible.NKI.Types
+import Plausible.IR.Prelude
 
 import Plausible.New.Chamelean
--- Plausible/IR/PlausibleIR.lean
+++
@@ -1,9 +0,0 @@
-import Plausible.IR.Examples
-import Plausible.IR.Extractor
-import Plausible.IR.Prelude
-import Plausible.IR.Prototype
-import Plausible.IR.Action
-import Plausible.IR.Constructor
-import Plausible.IR.Backtrack
-import Plausible.IR.Generator
-import Plausible.IR.Renaming
